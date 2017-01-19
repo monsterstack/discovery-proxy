8b10764f07e520df4928d9a2da90be0cfeb16f2d
zacharyrote <zachary.rote@gmail.com>
2017-01-19 14:01:04 -0500

    refactor exit handler

--- index.js
+++ index.js
@@ -26,27 +26,47 @@ const connect = (options, callback) => {
 }
 
 // @TODO: Proxy shouldn't have access to discovery-model repository..
-// Move this to discovery-service.
+// Should do a DELETE call to discoveryService
 const createErrorHandler = (serviceId, model) => {
   return function(options, err) {
     if (options.cleanup) {
       console.log('clean');
-      if(model) {
-        model.deleteService({id:serviceId}).then((service) => {
-          console.log(`Cleaned up Service ${service.id}`);
-          setTimeout(() => {
-            process.exit();
-          }, 500);
-        }).error((error) => {
-          console.log(`Service Delete failed ${serviceId}`);
-          console.log(error);
-          process.exit();
-        });
-      } else {
-        setTimeout(() => {
-          process.exit();
-        }, 500);
-      }
+      let payload = '';
+      let request = http.request({
+        host: 'localhost',
+        port: 7616,
+        path: 'api/v1/services/'+serviceId,
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(payload)
+        }
+      });
+
+
+      request.write(payload);
+      request.end();
+
+      setTimeout(() => {
+        process.exit();
+      }, 500);
+
+      // if(model) {
+      //   model.deleteService({id:serviceId}).then((service) => {
+      //     console.log(`Cleaned up Service ${service.id}`);
+      //     setTimeout(() => {
+      //       process.exit();
+      //     }, 500);
+      //   }).error((error) => {
+      //     console.log(`Service Delete failed ${serviceId}`);
+      //     console.log(error);
+      //     process.exit();
+      //   });
+      // } else {
+      //   setTimeout(() => {
+      //     process.exit();
+      //   }, 500);
+      // }
     }
 
     if (err) {
