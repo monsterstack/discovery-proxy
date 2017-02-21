6194e92bc6e9b02e622ff4f7b6dccd7988718b98
zacharyrote <zachary.rote@gmail.com>
2017-02-21 16:32:09 -0500

    discovery-proxy

--- libs/httpAgent.js
+++ libs/httpAgent.js
@@ -9,8 +9,23 @@ class HttpAgent {
     addResIntc(itc) {
         this.itcList.push(itc);
     }
-    
-     _startPerformance() {
+
+    responseHandler(callback) {
+        let self = this;
+        return (err, res) => {
+            if(res) {
+                res = self._formRes(res, p);
+            }
+
+            self.itcList.forEach((itc) => {
+                itc(res);
+            });
+            callback(err, res);
+        }
+
+    }
+
+    _startPerformance() {
         return {
             requestStart: Date.now()
         };
@@ -34,92 +49,38 @@ class HttpAgent {
     _get(path, headers, callback) {
         let self = this;
         let p = self._startPerformance();
-        needle.get(path, { headers: headers }, (err, res) => {
-            if(res) {
-                res = self._formRes(res, p);
-            }
-
-            self.itcList.forEach((itc) => {
-                itc(res);
-            });
-            callback(err, res);
-        });
+        needle.get(path, { headers: headers }, responseHandler(callback));
     }
 
     _patch(path, body, headers, callback) {
         let self = this;
         let p = self._startPerformance();
-        needle.patch(path, body, { headers: headers }, (err, res) => {
-            if(res) {
-                res = self._formRes(res, p);
-            }
-
-            self.itcList.forEach((itc) => {
-                itc(res);
-            });
-            callback(err, res);
-        });
+        needle.patch(path, body, { headers: headers }, responseHandler(callback));
     }
 
      _post(path, body, headers, callback) {
         let self = this;
         let p = self._startPerformance();
-        needle.post(path, body, { headers: headers }, (err, res) => {
-            if(res) {
-                res = self._formRes(res, p);
-            }
-
-            self.itcList.forEach((itc) => {
-                itc(res);
-            });
-            callback(err, res);
-        });
+        needle.post(path, body, { headers: headers }, responseHandler(callback));
     }
 
 
     _put(path, body, headers, callback) {
         let self = this;
         let p = self._startPerformance();
-        needle.put(path, body, { headers: headers }, (err, res) => {
-            if(res) {
-                res = self._formRes(res, p);
-            }
-
-            self.itcList.forEach((itc) => {
-                itc(res);
-            });
-            callback(err, res);
-        });
+        needle.put(path, body, { headers: headers }, responseHandler(callback));
     }
 
     _delete(path, body, headers, callback) {
         let self = this;
         let p = self._startPerformance();
-        needle.delete(path, body, { headers: headers }, (err, res) => {
-            if(res) {
-                res = self._formRes(res, p);
-            }
-
-            self.itcList.forEach((itc) => {
-                itc(res);
-            });
-            callback(err, res);
-        });
+        needle.delete(path, body, { headers: headers }, responseHandler(callback));
     }
 
     _head(path, headers, callback) {
         let self = this;
         let p = self._startPerformance();
-        needle.head(path, { headers: headers }, (err, res) => {
-            if(res) {
-                res = self._formRes(res, p);
-            }
-
-            self.itcList.forEach((itc) => {
-                itc(res);
-            });
-            callback(err, res);
-        });
+        needle.head(path, { headers: headers }, responseHandler(callback));
     }
 
 }
