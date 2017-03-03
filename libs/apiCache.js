2c57e5da26f218de986ce679863d94942bbad4a8
zacharyrote <zachary.rote@gmail.com>
2017-03-03 11:02:48 -0500

    stop using node-cache. it flattens obj and removes functions

--- libs/apiCache.js
+++ libs/apiCache.js
@@ -1,24 +1,27 @@
 'use strict';
-const NodeCache = require('node-cache');
 
 class ApiCache {
     constructor(options) {
         
         this.options = options || {};
         
-        this.apiCache = new NodeCache({ stdTTL: this.options.ttl || 15*60, checkperiod: this.options.checkperiod || 1*60 });
+        if(!options.ttl) {
+            options.ttl = 5*1000;
+        }
+
+        this.apiCache = {};
     }
 
     set(key, obj) {
         let self = this;
         let p = new Promise((resolve, reject) => {
-            self.apiCache.set(key, obj, (err, success) => {
-                if( !err && success ) {
-                    resolve();
-                } else {
-                    reject(err);
-                }
-            });
+            self.apiCache[key] = obj;
+            
+            setTimeout(() => {
+                delete self.apiCache[key];
+            }, self.options.ttl);
+
+            resolve();
         });
 
         return p;
@@ -28,17 +31,9 @@ class ApiCache {
     get(key) {
         let self = this;
         let p = new Promise((resolve, reject) => {
-            self.apiCache.get(key, (err, value) => {
-                if( !err ) {
-                    if(value == undefined) {
-                        resolve(null);
-                    } else {
-                        resolve(value);
-                    }
-                } else {
-                    reject(err);
-                }
-            });
+            let obj = self.apiCache[key];
+            if(obj) resolve(obj);
+            else resolve(null);
         });
 
         return p;
